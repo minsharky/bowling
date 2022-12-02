93c7e92510d4386bd6697f256f1bcf1bc24842c7
AkashDeoNU <103459323+AkashDeoNU@users.noreply.github.com>
2022-12-02 13:53:04 -0500

    Proper Scoring System Implemented. Check Pin.cs

--- Bowling/Assets/Scripts/Pin.cs
+++ Bowling/Assets/Scripts/Pin.cs
@@ -1,15 +1,30 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Pin : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private bool scored = false;
+
+    private float pinThreshold = -3.5f;
+
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     if (collision.gameObject.GetComponent<Ball>() != null)
+    //     {
+    //         // destroy after all of the pins stop moving
+    //         // add points to scoreboard
+    //         Scoreboard.ScorePoints(1);
+    //     }
+    // }
+
+    void Update()
     {
-        if (collision.gameObject.GetComponent<Ball>() != null) {
-            // destroy after all of the pins stop moving
-            // add points to scoreboard
+        if (transform.position.y < pinThreshold && !scored)
+        {
+            scored = true;
             Scoreboard.ScorePoints(1);
         }
+
     }
 }

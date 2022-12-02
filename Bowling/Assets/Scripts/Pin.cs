c891cb6c7a7adcb91ecb504b6d4f2739b1233700
AkashDeoNU <103459323+AkashDeoNU@users.noreply.github.com>
2022-12-02 12:39:24 -0500

    Revert "Bad attempt at scoring. If the ball collides with the pin, you get points. But, we need something else better - so the pin has to touch the ground."
    This reverts commit fd95bbe3ff3ad478cf6078331fec743346d1b9d3.

--- Bowling/Assets/Scripts/Pin.cs
+++ Bowling/Assets/Scripts/Pin.cs
@@ -6,10 +6,9 @@ public class Pin : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.GetComponent<Ball>() != null) {
-            // destroy after all of the pins stop moving
-            // add points to scoreboard
-            Scoreboard.ScorePoints(1);
+        if (collision.gameObject.name == "Player") { 
+        //destroy after all of the pins stop moving
+        //add points to scoreboard
         }
     }
 }
